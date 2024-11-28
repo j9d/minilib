a20f63b55c1e09e40813df9a2762a51c8f8445af
Jamie <jamiedimos@gmail.com>
2024-11-28 22:04:30 +1100

    Add basic view and some typing

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import home
+from .views import add, home
 
 urlpatterns = [
     path("", home),
+    path("add/", add),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,17 @@
 from django.contrib.auth.decorators import login_required
+from django.http import HttpRequest, HttpResponseNotAllowed
+from django.http.response import HttpResponse
 from django.template.response import TemplateResponse
 
 
 @login_required
-def home(request):
+def home(request: HttpRequest):
     return TemplateResponse(request, "home.html")
+
+
+@login_required
+def add(request: HttpRequest):
+    if request.method != "POST":
+        return HttpResponseNotAllowed(permitted_methods=["POST"])
+
+    return HttpResponse()
