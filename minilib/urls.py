6832cf6d9a82079a1ae417f8ef382e628cca48f0
Jamie <jamiedimos@gmail.com>
2024-11-28 20:50:44 +1100

    Tweak template styling

--- minilib/urls.py
+++ minilib/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("", include('core.urls')),
+    path("", include("core.urls")),
     path("admin/", admin.site.urls),
-    path("accounts/", include('django.contrib.auth.urls')),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
